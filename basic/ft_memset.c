e66148f83fb0932a9b393fb4c0ca39b71cbaf326
Raphael BALBOUS <rbalbous@e2r11p14.42.fr>
2018-05-03 15:54:15 +0200

    memset opti

--- basic/ft_memset.c
+++ basic/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: rbalbous <rbalbous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/30 19:59:06 by home              #+#    #+#             */
-/*   Updated: 2017/11/07 12:32:56 by rbalbous         ###   ########.fr       */
+/*   Updated: 2018/05/03 15:47:05 by rbalbous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,21 @@
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	char	*tmp;
+	uintmax_t		octet;
 
-	tmp = str;
-	while (0 < n--)
-		*(tmp++) = c;
+	octet = c | (c << 8);
+	octet |= (octet << 16);
+	octet |= (octet << 32);
+	while (n >= 8)
+	{
+		*((uintmax_t *)str) = octet;
+		str = (void*)((uintmax_t *)str + 1);
+		n -= 8;
+	}
+	while (n-- > 0)
+	{
+		*((unsigned char *)str) = c;
+		str = (void*)((unsigned char *)str + 1);
+	}
 	return (str);
 }
