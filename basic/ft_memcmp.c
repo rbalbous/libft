cb0821fc6637b3fd96ea736bf262e90da51ade59
Raphael BALBOUS <rbalbous@e2r11p14.42.fr>
2018-05-03 15:54:47 +0200

    added memcmp

--- basic/ft_memcmp.c
+++ basic/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rbalbous <rbalbous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 19:22:20 by rbalbous          #+#    #+#             */
-/*   Updated: 2017/11/08 15:36:31 by rbalbous         ###   ########.fr       */
+/*   Updated: 2018/05/03 15:50:47 by rbalbous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,11 @@
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	int				i;
-	unsigned char	*str1;
-	unsigned char	*str2;
-
-	str1 = (unsigned char*)s1;
-	str2 = (unsigned char*)s2;
 	i = 0;
 	while (n-- > 0)
 	{
-		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+		if (((unsigned char*)s1)[i] != ((unsigned char*)s2)[i])
+			return (((unsigned char*)s1)[i] - ((unsigned char*)s2)[i]);
 		i++;
 	}
 	return (0);
